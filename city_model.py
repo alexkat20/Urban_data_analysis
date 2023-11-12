c2414e77d490e92b92b32d8c004af2db883a1029
Daniil Zhembrovskiy <125507790+danil-zhembrovskii@users.noreply.github.com>
2023-11-12 17:43:54 +0300

    Revert "Alexkat branch"

--- city_model.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from CityGeoTools.metrics.data import CityInformationModel as BaseModel
-
-
-city_model = BaseModel.CityInformationModel(city_name="Kaliningrad", city_crs=4326)
-
-print(city_model.MobilityGraph)
-

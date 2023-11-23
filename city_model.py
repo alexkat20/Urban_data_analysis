643be38fbcf80a8a1802257557fa2cf920aea5e0
alexkat20 <43163320+alexkat20@users.noreply.github.com>
2023-11-23 10:13:21 +0300

    Update city_model.py

--- city_model.py
+++ city_model.py
@@ -5,12 +5,3 @@ from CityGeoTools.metrics.data import CityInformationModel as BaseModel
 city_model = BaseModel.CityInformationModel(city_name="Kaliningrad", city_crs=32634)
 
 print(city_model.MobilityGraph)
-
-city_model.update_layer(attr_name="Buildings", file_name="full_data.geojson")
-
-print(city_model.Buildings)
-
-city_model.update_layer(attr_name="Cafes", file_name="Urban_data_analysis/data/Инфраструктура досуга/Питание/cafe.geojson")
-
-
-city_model.methods.services_clusterization.message

50d248cd0d78ccb632805df4190105eda788c073
alexkat20 <alexkat20@rambler.ru>
2023-11-27 20:12:32 +0300

    Removed City_Model.py

--- City_Model.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import numpy as np
-from CityGeoTools.metrics.data import CityInformationModel as BaseModel
-
-city_model = BaseModel.CityInformationModel(city_name="Kaliningrad", city_crs=3857)
-
-print(city_model)
-
